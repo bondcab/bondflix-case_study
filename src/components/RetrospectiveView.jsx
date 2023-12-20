299353aa730c710b94695000231506d94b197182
bondcab <136003551+bondcab@users.noreply.github.com>
2023-12-20 16:15:22 +0000

    Update RetrospectiveView.jsx

--- src/components/RetrospectiveView.jsx
+++ src/components/RetrospectiveView.jsx
@@ -7,59 +7,65 @@ function RetrospectiveView() {
         <Container className="serverSideContainer" fluid>
           <div className="serverSideView">
             <div className="retrospectiveTitleContainer">
-              <h2 className="retrospectiveTitle">Retrospective</h2>
+              <h2 className="retroTitle">Retrospective</h2>
             </div>
 
             <Row>
-              <Col className="retrospectiveGridItem">
-                <div className="retrospectiveItemContainer">
-                  <div className="retroImageTitle">
-                    <div className="thumbsUpImage"></div>
-                    <h3 className="retrospectiveSubTitle">What went well?</h3>
-                  </div>
+              <Col sm={12} md={6}>
+                <div className="retrospectiveGridItem">
+                  <div className="retrospectiveItemContainer">
+                    <div className="retroImageTitle">
+                      <div className="thumbsUpImage"></div>
+                      <h3 className="retrospectiveSubTitle">What went well?</h3>
+                    </div>
 
-                  <div className="restAPIContainer">
-                    <p className="restAPIText">
-                      During this project I preferred working on the front end
-                      side more. Although I found React very challenging at
-                      first now it feels very intuitive.
-                    </p>
+                    <div className="restAPIContainer">
+                      <p className="retroText">
+                        During this project I preferred working on the front end
+                        side more. Although I found React very challenging at
+                        first now it feels very intuitive.
+                      </p>
+                    </div>
                   </div>
                 </div>
               </Col>
-              <Col className="retrospectiveGridItem">
-                <div className="retrospectiveItemContainer">
-                  <div className="retroImageTitle">
-                    <div className="thumbsDownImage"></div>
-                    <h3 className="retrospectiveSubTitle">
-                      What didn't go well?
-                    </h3>
-                  </div>
+              <Col sm={12} md={6}>
+                <div className="retrospectiveGridItem">
+                  <div className="retrospectiveItemContainer">
+                    <div className="retroImageTitle">
+                      <div className="thumbsDownImage"></div>
+                      <h3 className="retrospectiveSubTitle">
+                        What didn't go well?
+                      </h3>
+                    </div>
 
-                  <div className="restAPIContainer">
-                    <p className="restAPIText">
-                      The part I found most challenging was implementing server
-                      side authentication and security. This is a subject I plan
-                      to prioritise more in my current studies
-                    </p>
+                    <div className="restAPIContainer">
+                      <p className="retroText">
+                        The part I found most challenging was implementing
+                        server side authentication and security. This is a
+                        subject I plan to prioritise more in my current studies
+                      </p>
+                    </div>
                   </div>
                 </div>
               </Col>
             </Row>
             <Row>
-              <Col className="retrospectiveGridItem">
-                <div className="retrospectiveLastItemContainer">
-                  <div className="retroImageTitle">
-                    <div className="whatsNextImage"></div>
-                    <h3 className="retrospectiveSubTitle">Whats next?</h3>
-                  </div>
+              <Col>
+                <div className="retrospectiveGridItem">
+                  <div className="retrospectiveLastItemContainer">
+                    <div className="retroImageTitle">
+                      <div className="whatsNextImage"></div>
+                      <h3 className="retrospectiveSubTitle">Whats next?</h3>
+                    </div>
 
-                  <div className="restAPIContainer">
-                    <p className="retroText">
-                      I plan to try develop more applications using the MERN
-                      stack to better my understanding of all technologies
-                      involved
-                    </p>
+                    <div className="restAPIContainer">
+                      <p className="retroText">
+                        I plan to try develop more applications using the MERN
+                        stack to better my understanding of all technologies
+                        involved
+                      </p>
+                    </div>
                   </div>
                 </div>
               </Col>
